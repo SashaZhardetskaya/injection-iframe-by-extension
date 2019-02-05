21e4f00c46f5aa0fa07ed15456c5dd41b72f7781
zhara <zhara@zeoalliance.com>
2019-02-05 15:52:43 +0200

    changed CSP && manifest

--- extension/js/content.js
+++ extension/js/content.js
@@ -4,7 +4,7 @@ setTimeout(() => {
   console.log('SET TIMEOUT WORKED!');
   var iframe = document.createElement('iframe');
   // iframe.style.display = "none";
-  iframe.src = 'https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options';
+  iframe.src = 'http://recognition.10.55.39.17.xip.io';
   // document.body.appendChild(iframe);
   window.document.getElementById('login').appendChild(iframe)
-}, 4000);
+}, 2000);
--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const app = express();
 const path = require('path');
 const port = 9001;
 
-app.use(express.static(path.join(__dirname, 'pages')));
 app.use(function(req, res, next) {
-  // res.header("Content-Security-Policy", "script-src 'self' https://apis.google.com");
-  res.header("Content-Security-Policy", "frame-src 'self'");
+  res.header("Content-Security-Policy", "frame-src 'self'; script-src 'self' https://apis.google.com;");
+  // res.header("Content-Security-Policy", "frame-src 'self'");
+  // res.set("Content-Security-Policy", "default-src 'self';");
   res.header("X-Frame-Options", "deny");
   return next();
 });
+app.use(express.static(path.join(__dirname, 'pages')));
 
 app.listen(port);
 
