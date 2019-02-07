2ac4fe01ac710ad9c3366838902c13483eb60c22
zhara <zhara@zeoalliance.com>
2019-02-07 15:28:44 +0200

    inject extension's own page

--- extension/js/content.js
+++ extension/js/content.js
@@ -4,7 +4,8 @@ setTimeout(() => {
   console.log('SET TIMEOUT WORKED!');
   var iframe = document.createElement('iframe');
   // iframe.style.display = "none";
-  iframe.src = 'http://recognition.10.55.39.17.xip.io';
+  // iframe.src = 'http://recognition.10.55.39.17.xip.io';
+  iframe.style.display = 'chrome-extension://ffhpppcepenmeegpjndakheolopiafen/html/index.html';
   // document.body.appendChild(iframe);
-  window.document.getElementById('login').appendChild(iframe)
+  window.document.body.appendChild(iframe)
 }, 2000);
